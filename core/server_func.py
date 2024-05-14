e221979ffe7da7024c7d8ea6df1acd5668b0fe14
GsDeluxe <105081368+GsDeluxe@users.noreply.github.com>
2024-05-14 20:20:35 +0100

    Server Update
    Added Terminal UI
    Added TOR Hosting Functionality

--- core/server_func.py
+++ core/server_func.py
@@ -1,3 +1,4 @@
+
 import socket
 import struct
 import os
@@ -8,132 +9,135 @@ import rsa
 
 from core import cryptohandler
 
-
 class Server():
-    def __init__(self, host="127.0.0.1", port=1254, tor=False, password=None) -> None:
-        self.tor = tor
-        self.password = password
-        self.host = host
-        self.port = port
-        self.PUBLIC_KEY, self.PRIVATE_KEY = rsa.newkeys(2048)
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.cryptoHandler = cryptohandler.CryptoHandler()
-        self.client_dict = {
-                "CLIENT_OBJ": [],
-                "CLIENT_IPS": [],
-                "CLIENT_KEYS": [],
-                "CLIENT_NICKS": []
-            }
-        
-    def __start_tor(self):
-        SOCKS_PORT = 9050
-        SERVER_HOST = "127.0.0.1"
-        TOR_PATH = os.path.normpath(os.getcwd()+"\\tor\\tor.exe")
-        tor_process = stem.process.launch_tor_with_config(
-        config = {
-            'SocksPort': str(SOCKS_PORT),
-            'HiddenServiceDir': "./tor",
-            'HiddenServicePort': f'{str(self.port)} {SERVER_HOST}:{str(self.port)}'
-        },
-        init_msg_handler = lambda line: print(line) if re.search('Bootstrapped', line) else False,
-        tor_cmd = TOR_PATH
-        )
+	def __init__(self, host="127.0.0.1", port=1254, tor=False, password=None) -> None:
+		self.tor = tor
+		self.password = password
+		self.host = host
+		self.port = port
+		self.last_msg = ""
+		self.PUBLIC_KEY, self.PRIVATE_KEY = rsa.newkeys(2048)
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.cryptoHandler = cryptohandler.CryptoHandler()
+		self.client_dict = {
+				"CLIENT_OBJ": [],
+				"CLIENT_IPS": [],
+				"CLIENT_KEYS": [],
+				"CLIENT_NICKS": []
+			}
+		
+	def __start_tor(self):
+		SOCKS_PORT = 9050
+		SERVER_HOST = "127.0.0.1"
+		TOR_PATH = os.path.normpath(os.getcwd()+"\\tor\\tor.exe")
+		tor_process = stem.process.launch_tor_with_config(
+		config = {
+			'SocksPort': str(SOCKS_PORT),
+			'HiddenServiceDir': "./tor",
+			'HiddenServicePort': f'{str(self.port)} {SERVER_HOST}:{str(self.port)}'
+		},
+		init_msg_handler = lambda line: print(line) if re.search('Bootstrapped', line) else False,
+		tor_cmd = TOR_PATH
+		)
 
-        with open("tor/hostname", "r") as f:
-            hostname = f.read().strip()
-            f.close()
-        
-        return {"TOR_PROC": tor_process, "HOSTNAME": hostname}
-    
-    def __send_msg(self, sock, msg):
-        msg = struct.pack('>I', len(msg)) + msg
-        sock.sendall(msg)
+		with open("tor/hostname", "r") as f:
+			hostname = f.read().strip()
+			f.close()
+		
+		return {"TOR_PROC": tor_process, "HOSTNAME": hostname}
+	
+	def __send_msg(self, sock, msg):
+		msg = struct.pack('>I', len(msg)) + msg
+		sock.sendall(msg)
 
-    def __recv_msg(self, sock):
-        raw_msglen = self.__recvall(sock, 4)
-        if not raw_msglen:
-            return None
-        msglen = struct.unpack('>I', raw_msglen)[0]
-        return self.__recvall(sock, msglen)
+	def __recv_msg(self, sock):
+		raw_msglen = self.__recvall(sock, 4)
+		if not raw_msglen:
+			return None
+		msglen = struct.unpack('>I', raw_msglen)[0]
+		return self.__recvall(sock, msglen)
 
-    def __recvall(self, sock, n):
-        data = bytearray()
-        packet = sock.recv(n - len(data))
-        while len(data) < n:
-            if not packet:
-                return None
-            data.extend(packet)
-        return data
+	def __recvall(self, sock, n):
+		data = bytearray()
+		packet = sock.recv(n - len(data))
+		while len(data) < n:
+			if not packet:
+				return None
+			data.extend(packet)
+		return data
 
-    def send_encrypt(self, client_obj: socket.socket, data: bytes, pubkey: rsa.PublicKey):
-        key = self.cryptoHandler.generate_key(self.cryptoHandler.get_random_bytes())
-        key_encrypted = rsa.encrypt(pub_key=pubkey, message=key)
-        self.__send_msg(sock=client_obj, msg=key_encrypted)
-        
-        msg = self.cryptoHandler.encryption(key=key, data=data)
-        self.__send_msg(sock=client_obj, msg=msg)
+	def send_encrypt(self, client_obj: socket.socket, data: bytes, pubkey: rsa.PublicKey):
+		key = self.cryptoHandler.generate_key(self.cryptoHandler.get_random_bytes())
+		key_encrypted = rsa.encrypt(pub_key=pubkey, message=key)
+		self.__send_msg(sock=client_obj, msg=key_encrypted)
+		
+		msg = self.cryptoHandler.encryption(key=key, data=data)
+		self.__send_msg(sock=client_obj, msg=msg)
 
+	def recv_encrypt(self, client_obj: socket.socket, privkey: rsa.PrivateKey):
+		key = rsa.decrypt(crypto=self.__recv_msg(sock=client_obj), priv_key=privkey)
 
+		encrypted_msg = self.__recv_msg(sock=client_obj)
+		decrypted_msg = self.cryptoHandler.decryption(key=key, cipherdata=encrypted_msg)
+		return decrypted_msg
 
-    def recv_encrypt(self, client_obj: socket.socket, privkey: rsa.PrivateKey):
-        key = rsa.decrypt(crypto=self.__recv_msg(sock=client_obj), priv_key=privkey)
+	def broadcast_msg(self, data: bytes, nickname: str):
+		data = b"[" + nickname + b"]: " + data
+		self.last_msg = data.decode()
+		for index, client in enumerate(self.client_dict["CLIENT_OBJ"]):
+			self.send_encrypt(client_obj=client, data=data, pubkey=self.client_dict["CLIENT_KEYS"][index])
 
-        encrypted_msg = self.__recv_msg(sock=client_obj)
-        decrypted_msg = self.cryptoHandler.decryption(key=key, cipherdata=encrypted_msg)
-        return decrypted_msg
+	def __handle_client(self, client_obj: socket.socket, nickname: str):
+		self.broadcast_msg(data=f"{nickname.decode()} Has Joined".encode(), nickname=b"Server")
+		while True:
+			try:
+				msg = self.recv_encrypt(client_obj=client_obj, privkey=self.PRIVATE_KEY)
+				self.broadcast_msg(data=msg, nickname=nickname)
+			except:
+				index = self.client_dict["CLIENT_OBJ"].index(client_obj)
+				del self.client_dict["CLIENT_OBJ"][index]
+				del self.client_dict["CLIENT_IPS"][index]
+				del self.client_dict["CLIENT_KEYS"][index]
+				del self.client_dict["CLIENT_NICKS"][index]
 
-    def broadcast_msg(self, data: bytes, nickname: str):
-        for index, client in enumerate(self.client_dict["CLIENT_OBJ"]):
-            data_ = b"[" + nickname + b"]: " + data
-            self.send_encrypt(client_obj=client, data=data_, pubkey=self.client_dict["CLIENT_KEYS"][index])
+				self.broadcast_msg(data=f"{nickname.decode()} Has Left".encode(), nickname=b"Server")
+				break
 
-    def __handle_client(self, client_obj: socket.socket, nickname: str):
-        self.broadcast_msg(data=f"{nickname.decode()} Has Joined".encode(), nickname=b"Server")
-        while True:
-            try:
-                msg = self.recv_encrypt(client_obj=client_obj, privkey=self.PRIVATE_KEY)
-                self.broadcast_msg(data=msg, nickname=nickname)
-            except:
-                index = self.client_dict["CLIENT_OBJ"].index(client_obj)
-                del self.client_dict["CLIENT_OBJ"][index]
-                del self.client_dict["CLIENT_IPS"][index]
-                del self.client_dict["CLIENT_KEYS"][index]
-                del self.client_dict["CLIENT_NICKS"][index]
+	def __accept_thread(self, socket_obj: socket.socket) -> None:
+		while True:
+			socket_obj.listen()
+			client_obj, client_ip = socket_obj.accept()
 
-                self.broadcast_msg(data=f"{nickname.decode()} Has Left".encode(), nickname=b"Server")
-                break
+			# KEY EXCHANGE
+			client_key = rsa.PublicKey.load_pkcs1(client_obj.recv(20480))
+			client_obj.sendall(self.PUBLIC_KEY.save_pkcs1())
 
-    def __accept_thread(self, socket_obj: socket.socket) -> None:
-        while True:
-            socket_obj.listen()
-            client_obj, client_ip = socket_obj.accept()
+			# NICKNAME RECV
+			client_nick = self.recv_encrypt(client_obj=client_obj, privkey=self.PRIVATE_KEY)
+			
+			if client_nick.decode().lower() == "server":
+				client_nick = b"server_"
 
-            # KEY EXCHANGE
-            client_key = rsa.PublicKey.load_pkcs1(client_obj.recv(20480))
-            client_obj.sendall(self.PUBLIC_KEY.save_pkcs1())
+			
+			
+			self.client_dict["CLIENT_OBJ"].append(client_obj)
+			self.client_dict["CLIENT_IPS"].append(client_ip)
+			self.client_dict["CLIENT_KEYS"].append(client_key)
+			self.client_dict["CLIENT_NICKS"].append(client_nick)
 
-            # NICKNAME RECV
-            client_nick = self.recv_encrypt(client_obj=client_obj, privkey=self.PRIVATE_KEY)
-            client_nick.decode()
-            
-            self.client_dict["CLIENT_OBJ"].append(client_obj)
-            self.client_dict["CLIENT_IPS"].append(client_ip)
-            self.client_dict["CLIENT_KEYS"].append(client_key)
-            self.client_dict["CLIENT_NICKS"].append(client_nick)
-
-            threading.Thread(target=self.__handle_client, args=(client_obj, client_nick)).start()
-    
-    def create_socket(self) -> bool:
-        if self.tor:
-            self.host = "127.0.0.1"
-            self.tor_ret = self.__start_tor()
-            self.hostname = self.tor_ret["HOSTNAME"]
-            print(self.hostname)
-        try:
-            self.sock.bind((self.host, self.port))
-            threading.Thread(target=self.__accept_thread, args=(self.sock, ), daemon=True).start()
-            return True
-        except Exception as e:
-            print(e)
-            return False
+			threading.Thread(target=self.__handle_client, args=(client_obj, client_nick)).start()
+	
+	def create_socket(self) -> bool:
+		if self.tor:
+			self.host = "127.0.0.1"
+			self.tor_ret = self.__start_tor()
+			self.hostname = self.tor_ret["HOSTNAME"]
+			print(self.hostname)
+		try:
+			self.sock.bind((self.host, self.port))
+			threading.Thread(target=self.__accept_thread, args=(self.sock, ), daemon=True).start()
+			return True
+		except Exception as e:
+			print(e)
+			return False
 
--- server.py
+++ server.py
@@ -1,15 +1,150 @@
+
+from textual import work, on
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, TextArea, Input, LoadingIndicator, Static, Label, Button
+from textual.screen import Screen
+import asyncio
+import threading
+
 import os
 from core import server_func
 
-SERVER_IP = "127.0.0.1"
-SERVER_PORT = 1111
-TOR = False
+import time
 
-server = server_func.Server(host=SERVER_IP, port=SERVER_PORT, tor=TOR)
-if not server.create_socket():
-    print("[-] Error with Starting Server")
-    os._exit(0)
-else:
-    print("SERVER STARTED")
+def StartServer(host, port, tor):
+    time.sleep(1)
+    try:
+        server = server_func.Server(host=host, port=port, tor=tor)
+        res = server.create_socket()
+        if res:
+            return server
+        else:
+            return False
+    except:
+        return False
+    
+def chat_thread(server: server_func.Server, output_box: TextArea):
+    last_msg = ""
     while True:
-        cmd = input("Server >> ")
+        last_msg = server.last_msg
+        if last_msg:
+            output_box.text += last_msg + "\n"
+            server.last_msg = None
+            output_box.scroll_end(animate=False)
+        time.sleep(0.1)
+
+class BuildServer(Screen):
+    def __init__(self, host, port, tor, name: str | None = None, id: str | None = None, classes: str | None = None) -> None:
+        self.host = host
+        self.port = port
+        self.tor = tor
+        super().__init__(name, id, classes)
+
+    def compose(self):
+        yield LoadingIndicator()
+
+    @work(exclusive=True)
+    async def build_server(self):
+        loop = asyncio.get_event_loop()
+        res = await loop.run_in_executor(None, StartServer, self.host, self.port, self.tor)
+        self.dismiss(res)
+
+    def on_mount(self):
+        self.build_server()
+
+class ServerConsole(Screen):
+    def __init__(self, host, port, server: server_func.Server, name: str | None = None, id: str | None = None, classes: str | None = None) -> None:
+        self.host = host
+        self.port = port
+        self.server = server
+        super().__init__(name=name, id=id, classes=classes)
+    
+    def compose(self):
+        yield Header(show_clock="True")
+        yield Footer()
+
+        self.output_text_area = TextArea("", read_only=True, id="chat_output")
+        yield self.output_text_area
+
+
+        yield Input("", placeholder="[SERVER] Enter Message", id="chat_input")
+    
+    def on_mount(self):
+        if self.server.tor:
+            self.host = self.server.hostname
+        
+        self.output_text_area.text += f"[+] Server Online: {self.host}:{str(self.port)}" + "\n"
+
+        threading.Thread(target=chat_thread, args=(self.server, self.output_text_area, ), daemon=True).start()
+
+class YesNo(Screen[bool]):
+
+    def __init__(self, question: str) -> None:
+        self.question = question
+        super().__init__()
+
+    def compose(self) -> ComposeResult:
+        yield Label(self.question)
+        yield Button("Yes", id="yes")
+        yield Button("No", id="no")
+
+    @on(Button.Pressed, "#yes")
+    def handle_yes(self) -> None:
+        self.dismiss(True)  
+
+    @on(Button.Pressed, "#no")
+    def handle_no(self) -> None:
+        self.dismiss(False)
+
+class ServerInfo(Static):
+
+    def compose(self):
+        yield Label("Start Server", id="start_label", expand=True)
+        yield Input(placeholder="Host", id="host_input")
+        yield Input(placeholder="Port", id="port_input", type="integer")
+        yield Button("Start Server", id="start_button")
+
+class ServerApp(App[str]):
+    CSS_PATH = "css/server.tcss"
+    ENABLE_COMMAND_PALETTE = False
+
+    def compose(self):
+        yield ServerInfo()
+
+    async def on_input_submitted(self, event: Input.Submitted):
+        input_id = event.input.id
+        if input_id == "chat_input":
+            serverconsole = self.query_one(ServerConsole)
+            server = serverconsole.server
+            server.broadcast_msg(event.value.encode(), nickname=b"Server")
+            event.input.clear()
+
+    @work
+    async def on_button_pressed(self, event: Button.Pressed):
+        button_id = event.button.id
+        if button_id == "start_button":
+            ServerForm = self.query_one(ServerInfo)
+            host = ServerForm.get_child_by_id("host_input")
+            port = ServerForm.get_child_by_id("port_input")
+
+            if host.value == None or host.value == "" or port.value == "" or port.value == None:
+                self.notify("Error Missing Values", severity="error")
+                return
+
+            if await self.push_screen_wait(YesNo(question="Use TOR?")):
+                tor = True
+            else:
+                tor = False
+            
+
+            def server_start_handle(res):
+                if res != False and type(res) == server_func.Server:
+                    self.notify("Created Server")
+                    self.push_screen(ServerConsole(host=host.value, port=int(port.value), server=res))
+                else:
+                    self.notify("Error Creating Server", severity="error")
+
+            await self.push_screen(BuildServer(host=host.value, port=int(port.value), tor=tor), server_start_handle)
+            
+
+ServerApp().run()
